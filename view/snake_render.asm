59fe6b5fb9389570e69ffa0c0b26c4246f0e828c
Jacinto Mba Cantero <jacmba@gmailcom>
2020-03-31 00:46:02 +0100

    Render snake tail

--- view/snake_render.asm
+++ view/snake_render.asm
@@ -13,12 +13,32 @@ RenderSnake:
   stx $0203
   sty $0200
   lda #$00
+  ldx #$00
+  ldy #$00
 RenderTailLoop:
   cmp Size
   beq RenderTailDone
-  pha
-  rol a
-  rol a ;multiply by 4
-  ; ToDo continue copying position data
+  pha ;Backup tail part counter
+  lda Tail,x ;Load x coordinate
+  pha 
+  inx
+  lda Tail,x ;Load y coordinate
+  inx ;Skip direction field
+  sta $0208,y
+  iny
+  lda #$01 ;Set tail sprite
+  sta $0208,y
+  iny
+  lda #$02 ;Set tail palette
+  sta $0208,y
+  iny
+  pla
+  sta $0208,y
+  iny
+  pla
+  tax
+  inx
+  txa
+  jmp RenderTailLoop
 RenderTailDone:
   rts
